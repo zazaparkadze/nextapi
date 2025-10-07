72cc91112a5320fb2a5e14d892fe8e2bb52c6f79
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-10-07 21:53:02 +0300

    second

--- src/app/api/employees/route.js
+++ src/app/api/employees/route.js
@@ -1,7 +1,9 @@
-import Employee from "../../../../model/Employee";
+import Employee from "@/model/Employee";
 import { NextResponse } from "next/server";
+import connectToMongo from "../../../lib/connectToMongo";
 
 export async function GET() {
+  connectToMongo();
   const employees = await Employee.find();
   return NextResponse.json(employees);
 }
