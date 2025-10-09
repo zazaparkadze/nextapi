83080857847b7d3703b1db9b5351a203b5173791
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-10-09 10:31:43 +0300

    connecting from functions

--- src/lib/deleteEmployee.tsx
+++ src/lib/deleteEmployee.tsx
@@ -1,7 +1,9 @@
 "use server";
 import Employee from "@/model/Employee";
+import connectToMongo from "./connectToMongo";
 
 export default async function deleteEmployee(id: number) {
+  connectToMongo();
   const res = await Employee.deleteOne({ id });
 
   return JSON.stringify(res);
